aa0acb50026ac4b72330a0c97adc6f1478c2ffb7
Shivam8299 <shivammishra0381@gmail.com>
2025-07-26 23:28:47 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-400'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam, recusandae atque enim cum qui magni temporibus vero animi molestias neque ex, voluptatum asperiores ea excepturi, cupiditate doloribus ratione amet!
+    </div>
+  )
+}
+
+export default App
