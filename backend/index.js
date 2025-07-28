2b52071c89059e2971ea97c05448a866ba4dfbeb
Shivam8299 <shivammishra0381@gmail.com>
2025-07-28 12:50:37 +0530

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
-import express from 'express' 
+import express from 'express'
+import cors from 'cors' 
+import 'dotenv/config'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+const port = process.env.PORT || 4000;
+
+
+app.get('/', (req,res)=>{
+    res.send("welcome to backend")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is runing on port ${port}`)
+})
+
+
